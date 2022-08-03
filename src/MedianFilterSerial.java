3a4626556e207800e5accacc05e286cc4142b2cb
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-08-03 22:33:11 +0200

    add constructor and compute median

--- src/MedianFilterSerial.java
+++ src/MedianFilterSerial.java
@@ -1,3 +1,19 @@
-public class MedianFilterSerial {
+public class MedianFilterSerial extends Filter {
+
+    /**
+     * Constructs a new MedianFilterSerial object with the specified window width.  
+     * 
+     * @param windowWidth the width of the window to use for the filter
+     * @throws IllegalArgumentException if windowWidth is not odd 
+     * or if windowWidth is less than 3
+     * @see Filter#Filter(int)
+     */
+    public MedianFilterSerial(int windowWidth) {
+        super(windowWidth);
+    }
+
+    
+
+
     
 }
