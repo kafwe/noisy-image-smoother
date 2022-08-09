8ad25c6f7a62840ee7725a010a0cd4cf74bc891b
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-08-09 15:01:38 +0200

    Remove inheritance from serial mean filter

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -10,7 +10,8 @@ import java.awt.Color;
  * 
  * @author Jordy Kafwe
  */
-public class MeanFilterSerial extends Filter {
+public class MeanFilterSerial {
+    private int windowWidth;
 
     /**
      * Constructs a new MeanFilterSerial object with the specified window width.
@@ -18,26 +19,34 @@ public class MeanFilterSerial extends Filter {
      * @param windowWidth the width of the window to use for the filter
      * @throws IllegalArgumentException if windowWidth is not odd or if 
      * windowWidth is less than 3
-     * @see Filter#Filter(int)
      */
     public MeanFilterSerial(int windowWidth) {
-        super(windowWidth);
+        setWindowWidth(windowWidth);
     }
 
     /**
-     * Computes the mean of the neighbouring pixels in the specified window.
+     * Returns the window width of the filter.
      * 
-     * @param values the values of the neighbouring pixels
-     * @return the int representing the mean of the neighbouring pixels
+     * @return the int representing the window width
      */
-    private int computeMean(int[] values) {
-        int sum = 0;
+    public int getWindowWidth() {
+        return windowWidth;
+    }
 
-        for (int value : values) {
-            sum += value;
+    /**
+     * Sets the window width of the filter.
+     * 
+     * @param windowWidth the width of the window to use for the filter. 
+     * @throws IllegalArgumentException if windowWidth is not odd or if 
+     * windowWidth is less than 3
+     */
+    public void setWindowWidth(int windowWidth) {
+        if (windowWidth % 2 == 0 || windowWidth < 3) {
+            throw new IllegalArgumentException(
+                "Window width must be odd and greater than 2");
         }
 
-        return sum/values.length;
+        this.windowWidth = windowWidth;
     }
 
     /**
@@ -45,41 +54,33 @@ public class MeanFilterSerial extends Filter {
      * 
      * @param image the image to apply the filter to
      * @return the filtered image
-     * @see Filter#apply(BufferedImage)
      */
-    @Override
     public BufferedImage apply(BufferedImage image) {
+        int windowSize = windowWidth * windowWidth;
         int width = image.getWidth();
         int height = image.getHeight();
         BufferedImage filteredImage = new BufferedImage(width, height, image.getType());
 
         // iterate through each pixel in the image
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                int[] redValues = new int[getWindowWidth() * getWindowWidth()];
-                int[] greenValues = new int[getWindowWidth() * getWindowWidth()];
-                int[] blueValues = new int[getWindowWidth() * getWindowWidth()];
-                
-                int index = 0;
+        for (int x = windowWidth/2 ; x < width - windowWidth/2; x++) {
+            for (int y = windowWidth/2; y < height - windowWidth/2; y++) {
+                int redValues = 0;
+                int greenValues = 0;
+                int blueValues = 0;
 
                 // iterate through each pixel in the window
                 for (int i = x - (getWindowWidth() / 2); i <= x + (getWindowWidth() / 2); i++) {
                     for (int j = y - (getWindowWidth() / 2); j <= y + (getWindowWidth() / 2); j++) {
-                        boolean inBounds = i >= 0 && i < width && j >= 0 && j < height;
-                        if (inBounds) {
-                            int rgb = image.getRGB(i, j);
-                            Color pixel = new Color(rgb);
-                            redValues[index] = pixel.getRed();
-                            greenValues[index] = pixel.getGreen();
-                            blueValues[index] = pixel.getBlue();
-                            index++;
-                        }
+                        int pixel = image.getRGB(i, j);
+                        redValues += pixel >> 16 & 0xFF;
+                        greenValues += pixel >> 8 & 0xFF;
+                        blueValues += pixel & 0xFF;
                     }
                 }
 
-                int red = computeMean(redValues);
-                int green = computeMean(greenValues);
-                int blue = computeMean(blueValues);
+                int red = redValues/windowSize;
+                int green = greenValues/windowSize;
+                int blue = blueValues/windowSize;
                 int filteredPixel = new Color(red, green, blue).getRGB();
                 
                 filteredImage.setRGB(x, y, filteredPixel);
