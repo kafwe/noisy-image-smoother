050609190490bd67895526cafcee46310d3c13ec
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-08-09 17:40:19 +0200

    add mean filter parallel skeleton

--- src/MeanFilterParallel.java
+++ src/MeanFilterParallel.java
@@ -1,3 +1,28 @@
-public class MeanFilterParallel {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import javax.imageio.ImageIO;
+
+public class MeanFilterParallel extends RecursiveAction {
+    
+    public MeanFilterParallel() {
+    }
+
+    protected void computeDirectly() {
+    }
+
+    @Override
+    protected void compute() {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static BufferedImage smooth(BufferedImage image) {
+        
+    }
     
 }
